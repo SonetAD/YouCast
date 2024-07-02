c81ad951c4e92fbdd703f63ad94430c40037c14d
Sonet Adhikary <sonet.ad101@gmail.com>
2024-07-02 11:00:04 +0800

    Modified server

--- server/search_youtube.js
+++ server/search_youtube.js
@@ -3,12 +3,10 @@ const {youtube} = require('scrape-youtube');
 const search = async userSearch => {
   try {
     const {videos} = await youtube.search(userSearch);
-    console.log(videos[0]);
     return videos;
   } catch (err) {
     throw new Error('Error searching on youtube');
   }
 };
 
-search('justin biber baby');
 module.exports = search;
