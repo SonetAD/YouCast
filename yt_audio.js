cff3892f1522d44c1a12b60ef388459145cba400
Sonet Adhikary <sonet.ad101@gmail.com>
2024-07-10 10:29:41 +0800

    Debug server build

--- yt_audio.js
+++ yt_audio.js
@@ -1,31 +1,63 @@
 const ytdl = require('ytdl-core');
+const axios = require('axios');
 
 const ytAudio = async (vId, res) => {
-  try {
-    const url = `http://www.youtube.com/watch?v=${vId}`;
-    console.log('Setting header');
-    res.setHeader('Content-Type', 'audio/mpeg');
+  const url = `http://www.youtube.com/watch?v=${vId}`;
 
-    console.log('Fetching video');
-    const stream = ytdl(url, {
-      filter: 'audioonly',
-      // headers: {
-      //   'User-Agent':
-      //     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-      // },
-    });
+  const fetchAudioStream = async () => {
+    try {
+      console.log('Setting header');
+      res.setHeader('Content-Type', 'audio/mpeg');
 
-    stream.on('error', (err) => {
-      console.error('Stream error:', err);
-      res.status(500).send('Error streaming audio');
-    });
+      console.log('Fetching video');
+      const stream = ytdl(url, {
+        filter: 'audioonly',
+        headers: {
+          'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        },
+      });
 
-    stream.pipe(res);
-    console.log('Streaming audio');
-  } catch (err) {
-    console.error('Error:', err);
-    res.status(500).send('Error processing request');
-  }
+      stream.on('error', (err) => {
+        console.error('Stream error:', err);
+        res.status(500).send('Error streaming audio');
+      });
+
+      stream.pipe(res);
+      console.log('Streaming audio');
+    } catch (err) {
+      console.error('Error:', err);
+      res.status(500).send('Error processing request');
+    }
+  };
+
+  // Retry mechanism with exponential backoff
+  let attemptCount = 0;
+  const maxAttempts = 3; // Maximum number of retry attempts
+
+  const retryFetchAudioStream = async () => {
+    attemptCount++;
+    if (attemptCount > maxAttempts) {
+      console.error('Max retry attempts reached');
+      return;
+    }
+
+    try {
+      await fetchAudioStream();
+    } catch (error) {
+      if (error.message.includes('MinigetError: Status code: 403')) {
+        console.log(`Retrying due to rate limit... Attempt ${attemptCount}`);
+        await new Promise((resolve) =>
+          setTimeout(resolve, Math.pow(2, attemptCount) * 1000)
+        );
+        return retryFetchAudioStream(); // Retry the request
+      } else {
+        throw error;
+      }
+    }
+  };
+
+  retryFetchAudioStream();
 };
 
 module.exports = ytAudio;
